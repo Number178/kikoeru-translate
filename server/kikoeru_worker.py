9512cefc1df27a08e6b03813db6ac1331217e7d4
number17 <753090084@qq.com>
2023-11-25 16:28:16 +0800

    kikoeru翻译任务功能补全

--- server/kikoeru_worker.py
+++ server/kikoeru_worker.py
@@ -5,6 +5,8 @@ import time
 import datetime
 import server.common as common
 import requests
+import math
+from transcribe import WhisperModel
 
 db_dir = common.getDbDir()
 task_file_path = common.getTaskFilePath()
@@ -15,6 +17,7 @@ kikoeru_url = common.getKikoeruUrl()
 kikoeru_user = common.getKikoeruUser()
 kikoeru_password = common.getKikoeruPassword()
 is_need_auth = False
+model: WhisperModel = None
 
 # 使用session进行通信，保存token，每一次运行前检查token是否失效，如果失效，需要重新登陆验证
 def setupSession(session:requests.Session, token:str):
@@ -65,16 +68,19 @@ def loginKikoeru(url:str, user:str, password:str)->str:
 
 # return [success, not_task_can_acquire, task]
 def acquireTask(url:str)->Tuple[bool, bool, Dict]:
-    res = session.post(
-        f"{url}/api/lyric/translate/acquire",
-        {
-            "worker_name": worker_name,
-        }
-    )
-    # print("acquire task response: ", res.status_code)
-    no_task_can_acquire = res.status_code == 404
-    success = res.status_code == 200
-    data = res.json()
+    try:
+        res = session.post(
+            f"{url}/api/lyric/translate/acquire",
+            {
+                "worker_name": worker_name,
+            }
+        )
+        # print("acquire task response: ", res.status_code)
+        no_task_can_acquire = res.status_code == 404
+        success = res.status_code == 200
+        data = res.json()
+    except:
+        return [False, True, None]
     # print(data)
     return (success, no_task_can_acquire, data)
 
@@ -131,11 +137,54 @@ def saveTaskToFile(task:Dict):
     with open(task_file_path, "w", encoding="utf8") as f:
         json.dump(task, f, indent=4)
 
+def format_seconds(
+    seconds: float,
+    decimal_marker: str = ".",
+) -> str:
+    assert seconds >= 0, "non-negative timestamp expected"
+
+    just_seconds = math.floor(seconds) % 60
+    just_hundredths_seconds = math.floor(100 * (seconds - math.floor(seconds))) # lrc 秒数以下用0-99显示剩余数位，而不是毫秒，所以这里乘以100, ref: https://en.wikipedia.org/wiki/LRC_(file_format)
+    just_minutes = math.floor(seconds / 60)
+    return (
+        f"[{just_minutes:02d}:{just_seconds:02d}{decimal_marker}{just_hundredths_seconds:02d}]"
+    )
+
+# output complete lrc contents joined by '\n'
+def transcribe_audio(audio_path:str)->str:
+    global model
+    segments, _ = model.transcribe(task="transcribe", audio=audio_path, language="zh")
+    return "\n".join(
+        map(
+            lambda segment: f"{format_seconds(segment.start)} {segment.text}",
+            segments,
+        )
+    )
+
+def checkTaskIsDelete(task:Dict)->bool:
+    try:
+        r = session.get(f"{kikoeru_url}/api/lyric/translate/get", params={
+            "id": task["id"],
+        }, stream=True)
+        if r.status_code == 200:
+            data = r.json()
+            print("get task status, data = ", data)
+            return 'task' not in data
+    except Exception as e:
+        print("检查任务状态失败：", e)
+        raise e
+
 # task = {id: 0, secret: "777f7f7f77fd7"}
 def processTask(task):
     print("存储task信息到本地文件中") # 目前一个worker一次只处理一个task
-    # 当进行处理的时候，通过一个本地文件
+    # 当进行处理的时候，通过一个本地文件记录，方便重启翻译服务器后继续前面的任务
     saveTaskToFile(task)
+
+    # 处理前，获取一次任务状态，如果任务被删除了的话，则不处理，直接返回
+    if checkTaskIsDelete(task):
+        print("服务器上的翻译任务已被删除，跳过当前任务")
+        os.unlink(task_file_path)
+        return
 
     if 'audio_file_name' not in task:
         print("下载音频文件")
@@ -147,41 +196,20 @@ def processTask(task):
         audio_file_name = task['audio_file_name']
 
     audio_file_path = os.path.join(input_audio_dir, audio_file_name)
-    print("音频文件下载至：", audio_file_path)
-    
-    for i in range(10):
-        time.sleep(0.5)
-        updateTaskStatus(task, f"翻译进度: {(i+1)*10}%")
+    print("音频文件位于：", audio_file_path)
 
-    finishTask(task, True, """
-[00:00.00] 啊啦，有个可爱的孩子迷上了你
-[00:05.16] 啊，没关系的哦
-[00:07.96] 不是你搞错了房间
-[00:10.72] 是哥哥你偶尔就进了这间房间
-[00:14.10] 这间房间是只能和想做色色的事的人和我们精灵连接的唯一的地方
-[00:25.92] 也就是说，哥哥现在是世界上最焦躁的状态
-[00:34.92] 可以进来哦
-[00:37.68] 请让我喝茶，慢慢说明一下
-[00:42.92] 要喝什么？
-[00:45.92] 这样啊
-[00:47.56] 那么，请让我准备精灵特制的红茶
-[00:53.76] 那么，请坐到床上去
-[01:08.20] 哥哥，一副非常可爱的表情呢
-[01:15.08] 刚才也说过了，真是太可爱了
-[01:19.88] 虽然我已经在这里做了好几年下流的事了
-[01:28.52] 但可能还是第一次体验到这么想吃的人
-[01:40.80] 红茶我拿来了，请吧
-[01:48.76] 那么，再次欢迎来到异世界
-[01:53.92] 关于本店的系统的事情，请允许我为您说明
-[01:59.96] 本店不会付您任何钱
-[02:05.00] 相对的，在玩法的最后
-[02:07.60] 会收下您浓稠的精液
-[02:11.60] 那个时候，请您尽情的射出浓稠的精液吧
-[02:19.30] 如果客人在中途射精的话，我们会接受惩罚
-[02:27.84] 如果没有问题的话，我想就这样给您按摩
-[02:34.84] 可以吗？
-[02:39.68] 竟然是这么快就答应了，真可靠啊
-""")
+    success = False
+    lrc_content = ""
+    try:
+        print("翻译中...")
+        lrc_content = transcribe_audio(audio_file_path)
+        success = True
+        print("翻译成功")
+    except Exception as e:
+        print("transcripting error, ", e)
+        success = False
+        
+    finishTask(task, success, lrc_content)
 
     print(" 任务完成，删除本地记录")
     os.unlink(audio_file_path)
@@ -198,6 +226,15 @@ def clearOldTaskAtStartup():
         task = json.load(f)
     processTask(task)
 
+def load_model():
+    print("load model start")
+    global model
+    model_path = common.getModelPath()
+    device = common.getTranscribeDevice()
+    compute_type = "default"
+    model = WhisperModel(model_path, device=device, compute_type=compute_type)
+    print("load model finished, start background loop, waiting for transcribe task")
+
 def main():
     print("hello world: ")
     print("kikoeru_url = ", kikoeru_url)
@@ -206,6 +243,7 @@ def main():
     
     global is_need_auth
     global kikoeru_token
+    global model
 
     is_need_auth = checkKikoeruAuth(kikoeru_url)
 
@@ -214,6 +252,8 @@ def main():
         if kikoeru_token != "":
             common.saveToken(kikoeru_token)
             setupSession(session, kikoeru_token)
+
+    load_model()
 
     clearOldTaskAtStartup()
     
